807d35eff9d7c6b4c24d2d935c0c56242c17f829
Simon Lindsay <simon@iseek.biz>
2021-02-23 11:46:12 +1030

    Tidying up things.

--- modules/se_ticket/src/Entity/Ticket.php
+++ modules/se_ticket/src/Entity/Ticket.php
@@ -220,10 +220,7 @@ class Ticket extends RevisionableContentEntityBase implements TicketInterface {
       ->setLabel(t('Name'))
       ->setDescription(t('The name of the Ticket entity.'))
       ->setRevisionable(TRUE)
-      ->setSettings([
-        'max_length' => 50,
-        'text_processing' => 0,
-      ])
+      ->setSetting('max_length', 128)
       ->setDefaultValue('')
       ->setDisplayOptions('view', [
         'label' => 'above',
