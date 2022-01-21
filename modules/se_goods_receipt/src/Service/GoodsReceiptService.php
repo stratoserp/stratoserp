18989cec63c7fcbb01a7c500ac6358c47ca0b8c5
Simon Lindsay <simon@iseek.biz>
2022-01-22 07:38:10 +1030

    Fix missed variable rename.

--- modules/se_goods_receipt/src/Service/GoodsReceiptService.php
+++ modules/se_goods_receipt/src/Service/GoodsReceiptService.php
@@ -6,12 +6,11 @@ namespace Drupal\se_goods_receipt\Service;
 
 use Drupal\se_goods_receipt\Entity\GoodsReceipt;
 use Drupal\se_item\Entity\Item;
-use Drupal\stratoserp\Constants;
 
 class GoodsReceiptService {
- 
+
   public function createItems(GoodsReceipt $goodsReceipt) {
-    foreach ($entity->se_item_lines as $index => $itemLine) {
+    foreach ($goodsReceipt->se_item_lines as $index => $itemLine) {
       if (!empty($itemLine->serial)) {
         /** @var \Drupal\se_item\Entity\Item $item */
         if ($item = Item::load($itemLine->target_id)) {
@@ -22,27 +21,27 @@ class GoodsReceiptService {
               ->set('se_it_ref', $item->id())
               ->save();
 
-            $entity->se_item_lines[$index]->target_id = $newItem->id();
+            $goodsReceipt->se_item_lines[$index]->target_id = $newItem->id();
           }
         }
       }
     }
   }
-  
+
   public function updateFields(GoodsReceipt $goodsReceipt) {
-    foreach ($entity->se_item_lines as $itemLine) {
+    foreach ($goodsReceipt->se_item_lines as $itemLine) {
       /** @var \Drupal\se_item\Entity\Item $item */
       if ($item = Item::load($itemLine->target_id)) {
         if ($item->bundle() !== 'se_stock') {
           continue;
         }
         $item
-          ->set('se_gr_ref', $entity->id())
-          ->set('se_po_ref', $entity->se_po_ref->target_id)
+          ->set('se_gr_ref', $goodsReceipt->id())
+          ->set('se_po_ref', $goodsReceipt->se_po_ref->target_id)
           ->set('se_cost_price', $itemLine->price)
           ->save();
       }
     }
   }
-  
+
 }
