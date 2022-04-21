1cd08623d3389d06e27155e5aade38d1ba4893ee
Simon Lindsay <simon@iseek.biz>
2022-04-21 20:51:12 +0930

    Laying out fields.

--- modules/se_item/src/Form/ItemForm.php
+++ modules/se_item/src/Form/ItemForm.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\se_item\Form;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\stratoserp\Form\StratosContentEntityForm;
 
 /**
@@ -13,5 +14,27 @@ use Drupal\stratoserp\Form\StratosContentEntityForm;
  */
 class ItemForm extends StratosContentEntityForm {
 
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+
+    if ($this->entity->isNew()) {
+      $temp = 1;
+
+      $this->hideFields($form, [
+        'se_po_ref', 'se_gr_ref', 'se_in_ref',
+        'se_it_ref',
+        'se_lost', 'se_sold', 'se_sale_date', 'se_sale_price',
+      ]);
+
+    }
+
+    return $form;
+  }
+
+  private function hideFields(array &$form, array $fields) {
+    foreach ($fields as $field) {
+      $form[$field]['#access'] = FALSE;
+    }
+  }
 
 }
