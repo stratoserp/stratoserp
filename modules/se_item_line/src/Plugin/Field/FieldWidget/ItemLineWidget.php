bb3b2168754ee288c5c423629ef0d17ea6d68a02
Simon Lindsay <simon@iseek.biz>
2022-11-12 13:41:21 +1030

    Move the recalculate function overrides to a better place.

--- modules/se_item_line/src/Plugin/Field/FieldWidget/ItemLineWidget.php
+++ modules/se_item_line/src/Plugin/Field/FieldWidget/ItemLineWidget.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\se_item_line\Plugin\Field\FieldWidget;
 
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Component\Utility\SortArray;
 use Drupal\Core\Datetime\DateFormatter;
 use Drupal\Core\Datetime\DrupalDateTime;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
@@ -12,7 +14,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\dynamic_entity_reference\Plugin\Field\FieldWidget\DynamicEntityReferenceWidget;
 use Drupal\se_accounting\Service\CurrencyFormatService;
-use Drupal\stratoserp\Traits\FormMultipleElementsTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -27,8 +28,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class ItemLineWidget extends DynamicEntityReferenceWidget {
-
-  use FormMultipleElementsTrait;
 
   /**
    * The entity manager.
@@ -209,6 +208,84 @@ class ItemLineWidget extends DynamicEntityReferenceWidget {
     }
 
     return $build;
+  }
+
+  /**
+   * Overrides Drupal\Core\Field\WidgetBase::formMultipleElements().
+   *
+   * Add the recalculate button.
+   */
+  protected function formMultipleElements(FieldItemListInterface $items, array &$form, FormStateInterface $form_state) {
+    $elements = parent::formMultipleElements($items, $form, $form_state);
+
+    $elements['recalculate'] = [
+      '#type' => 'submit',
+      '#name' => 'se_item_lines_recalculate',
+      '#value' => $this->t('Recalculate'),
+      '#attributes' => ['class' => ['field-recalculate-submit']],
+      '#limit_validation_errors' => $elements['add_more']['#limit_validation_errors'],
+      '#submit' => [[\Drupal\se_item_line\Controller\ItemLineController::class, 'updateFields']],
+      '#ajax' => [
+        'callback' => [\Drupal\se_item_line\Controller\ItemLineController::class, 'updateFields'],
+        'wrapper' => $elements['add_more']['#ajax']['wrapper'],
+        'effect' => 'slide',
+        'speed' => 'fast',
+        'progress' => 'none',
+      ],
+    ];
+
+    $elements['add_more']['#ajax']['speed'] = 'fast';
+    $elements['add_more']['#ajax']['progress'] = 'none';
+    $elements['add_more']['#attributes']['class'][] = 'btn btn-primary';
+
+    return $elements;
+  }
+
+  /**
+   * Overrides Drupal\Core\Field\WidgetBase::extractFormValues().
+   *
+   * Add support for the recalculate button.
+   */
+  public function extractFormValues(FieldItemListInterface $items, array $form, FormStateInterface $form_state) {
+    $field_name = $this->fieldDefinition->getName();
+
+    // Extract the values from $form_state->getValues().
+    $path = array_merge($form['#parents'], [$field_name]);
+    $key_exists = NULL;
+    $values = NestedArray::getValue($form_state->getValues(), $path, $key_exists);
+
+    if ($key_exists) {
+      // Account for drag-and-drop reordering if needed.
+      if (!$this->handlesMultipleValues()) {
+        // Remove the 'value' of the 'add more' button.
+        unset($values['add_more'], $values['recalculate']);
+
+        // The original delta, before drag-and-drop reordering, is needed to
+        // route errors to the correct form element.
+        foreach ($values as $delta => &$value) {
+          $value['_original_delta'] = $delta;
+        }
+
+        usort($values, function ($a, $b) {
+          return SortArray::sortByKeyInt($a, $b, '_weight');
+        });
+      }
+
+      // Let the widget massage the submitted values.
+      $values = $this->massageFormValues($values, $form, $form_state);
+
+      // Assign the values and remove the empty ones.
+      $items->setValue($values);
+      $items->filterEmptyItems();
+
+      // Put delta mapping in $form_state, so that flagErrors() can use it.
+      $field_state = static::getWidgetState($form['#parents'], $field_name, $form_state);
+      foreach ($items as $delta => $item) {
+        $field_state['original_deltas'][$delta] = $item->_original_delta ?? $delta;
+        unset($item->_original_delta, $item->_weight, $item->_actions);
+      }
+      static::setWidgetState($form['#parents'], $field_name, $form_state, $field_state);
+    }
   }
 
   /**
